99464a74fe33803c7ee8cbe0b22c0b9b90df69eb
Alexandra King <aking28@me.com>
2017-11-02 14:30:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,47 @@
 const app = "I don't do much."
+
+var kittens = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendKitten(thing){
+  kittens.push(thing);
+  return kittens
+
+}
+
+function destructivelyPrependKitten(name){
+  kittens.unshift(name);
+  return kittens
+}
+
+function destructivelyRemoveLastKitten(name){
+  kittens.pop(name);
+  return kittens
+}
+
+
+function destructivelyRemoveFirstKitten(name){
+  kittens.shift(name);
+  return kittens
+}
+
+
+
+var kittens = ["Milo","Otis", "Garfield"]
+
+function appendKitten(name){
+  var newkittens = [...kittens,name]
+  return newkittens
+
+}
+
+function prependKitten(name){
+  var newkittens = [name,...kittens]
+  return newkittens
+}
+
+var kittens = ["Milo","Otis", "Garfield"]
+
+function removeLastKitten(name){
+  var newkittens = kittens.slice(-1)
+  return newkittens
+}
